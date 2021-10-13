c49d68e134fab7f87011bc2bdadf8fd83a877daf
molchanova-anna <molchanova.aw@gmail.com>
2021-10-13 22:43:40 +0300

    review results

--- homework_04/blog_project/config.py
+++ homework_04/blog_project/config.py
@@ -3,4 +3,4 @@ import os
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://user:password@localhost:5432/blog_project"
 
 # Флаг очищать ли таблицы перед загрузкой
-DO_CLEAR_TABLES = True
+DO_CLEAR_TABLES = False
